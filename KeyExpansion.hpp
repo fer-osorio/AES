cce6ee0a49dad8329fc2a1f152118c5cf7afa2d6
Fernando Osorio <alexis.fernando.osorio.sarabio@gmail.com>
2023-02-27 23:01:26 -0600

    Arrays were made contants.

--- KeyExpansion.hpp
+++ KeyExpansion.hpp
@@ -2,16 +2,17 @@
 
 // -Notice that the value of the left most byte 
 //  in polynomial form is Rcon[i] == 2^i.
-byte Rcon[] = { 0x01000000,
-						 		0x02000000,
-							 	0x04000000,
-								 0x08000000,
- 								0x10000000,
-	 							0x20000000,
-		 						0x40000000,
-			 					0x80000000,
-				 				0x1B000000,
-					 			0x36000000 }
+byte Rcon[] =
+{ 0x01000000,
+	0x02000000,
+	0x04000000,
+  0x08000000,
+  0x10000000,
+	0x20000000,
+	0x40000000,
+	0x80000000,
+	0x1B000000,
+	0x36000000 };
 
 // -Standard rotation of bits to the left.
 ui32 RotLeft(ui32 word, byte n);
